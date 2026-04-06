a9042cd640185776a29558e99dc80e946d72eecd
Soney <sonephyo7777777@gmail.com>
2026-04-05 21:18:29 -0400

    feat: expose auth and worklog in aws

--- infra/main.tf
+++ infra/main.tf
@@ -43,6 +43,22 @@ resource "aws_security_group" "app_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  ingress {
+  description = "Backend worklog"
+  from_port   = 7001
+  to_port     = 7001
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+ingress {
+  description = "Backend auth"
+  from_port   = 7002
+  to_port     = 7002
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
 
   egress {
     from_port   = 0
